8ec44383ca12b509f0a676912f98845f3c24806d
trhi <terhim@gmail.com>
2023-03-24 11:39:56 +0000

    Add voice recordings, add menu (text only). Set sound+text as default at start. Poem is still linear.

--- dyeing.js
+++ dyeing.js
@@ -1,8 +1,10 @@
 let sliverSize = 10, graypoint = 250; //150
 let xover = 6/8, yover = 8/10;
-let observations, slivers = [], clicks = -1, iterator = 0, clicking = true, text = true, sound = false; //clicks=-1 to make it easier to work with the array indexes..
+let observations, slivers = [], clicks = -1, iterator = 0, clicking = true, text = true, sound = true, autoplay = false, playing=false, playpoem = false; //clicks=-1 to make it easier to work with the array indexes..
 let chosenBranch;
 let branchIterator = 0;
+let playpoemCounter = 0;
+let autoplayCounter = 0;
 
 //change name to:
 // gray hairs
@@ -41,7 +43,7 @@ function setup() {
   if ( window.innerWidth >= 992 ){
     xover = 9/10; //allow clicks further into the right side of the screen for larger screens
   } else {
-    sliverSize = window.innerWidth/20; //if small screen, tweak sliverSize to make tappable
+    sliverSize = window.innerWidth/27; // was /20 if small screen, tweak sliverSize to make tappable
   }
 
   xover *= windowWidth; //calculate the sliver of screen where text should not be displayed
@@ -51,12 +53,15 @@ function setup() {
   let sliversY = windowHeight/(sliverSize*2); //how many follicles fit vertically?
 
   for (let i=0; i<sliversY+1; i++){
-    for (let j=0; j<sliversX+1;j++){
+    for (let j=0; j<sliversX;j++){
       //create an array of hair follicles
       //with x and y coordinates that populate screen
-      slivers.push( new sliver(2*sliverSize*j + sliverSize/2, 2*sliverSize*i + sliverSize/2) );
+      slivers.push( new sliver( 2*sliverSize*j + sliverSize/2, 2*sliverSize*i + sliverSize/2 ) );
+      // 2.5*sliverSize*j makes them look like strands of hair
+      //it was  + sliverSize/2 for both x and y in order to make the first and last hairs go off screen
     }
   }
+
 
 //TODO: audio on/off button, play observations as recorded voice and/or text -> moved to audio.js
 
@@ -82,7 +87,80 @@ function setup() {
 
 }
 
+function readMe() {
+
+  if( autoplay ){
+
+    let autoplaying;
+
+    if ( !playpoem ){
+      autoplaying = random(slivers); //chooses random slivers to play
+    } else {
+      if ( autoplayCounter == slivers.length ){
+        autoplayCounter = 0;
+      }
+      autoplaying = slivers[autoplayCounter];
+      autoplayCounter++;
+    }
+    //console.log("chose this one:", autoplaying);
+    if (autoplaying.life > 250){
+      //console.log("I am gray!");
+      //autoplaying.fill = 'pink';
+      autoplaying.clicked();
+      playing = true;
+      setTimeout( readMe , 2000 );
+    } else {
+      //console.log("I am not gray!");
+      readMe();
+    }
+  }
+
+} //close readMe
+
+function toggleTrueFalse(variable, value) {
+
+  //console.log("I just received:", variable, "and it is:", value);
+  if( value ){
+    eval(variable + " = " + !value + ";");
+    if( variable === 'text'){
+      console.log("changing text");
+      $('text').html("CHANGED");
+    }
+    if( variable === 'paintbucket'){
+      console.log("changing text");
+      $('text').html("<s>paintbucket</s>");
+    }
+    //console.log("I received:", variable, "and set it to:", !value);
+  } else {
+    eval(variable + " = " + !value + ";");
+    if( variable === 'text' ){
+      $('text').html("Abc");
+    }
+    //console.log("I received:", variable, "and set it to:", !value);
+  }
+}
+
+function paintBucket(){
+  console.log("painting hairs");
+  for( let i=0; i < slivers.length; i++ ){
+    slivers[i].fill = 0;
+    slivers[i].life = random(0, graypoint-10);
+   } //let's you tap black hairs (because they are aged even though they are black..)
+  //for( let i=0; i < slivers.length; i++ ){ slivers[i].fill = 0; slivers[i].life = random(0, graypoint-50); } //effectively silences the poem
+}
+
+
 function draw() {
+
+  if( autoplay == false ){
+    playing = false;
+  }
+
+  //CANNOT BE LIKE THIS... this will call the function at EVERY draw cycle....
+  if ( clicks > -1 && autoplay && !playing){
+    readMe();
+  }
+
 
   //for showing the user how far along they are in the reading of the work:
   /*
@@ -94,10 +172,16 @@ function draw() {
   */
 
   if( frameCount == 4 ){ //show instruction after person opens work
-    let p = createP("<em> wait for the first gray hair, then tap it </em>");
+    let p;
+    if ( autoplay ){
+      p = createP("<em> wait for the first gray hair </em>");
+    } else {
+      p = createP("<em> wait for the first gray hair, then tap it </em>");
+    }
     p.position(windowWidth/10, windowHeight/10);
     $( "p" ).fadeOut( 10000 );
   }
+
   clear();
   drawSlivers();
   // what color is the pixel/hair that the user is mousing over?
@@ -121,7 +205,7 @@ function drawSlivers() {
 function mousePressed() {
   //console.log("mouse pressed");
   //upon touchStarted, check which hair was clicked by going through the array of hairs:
-  if ( clicking && clicks <= observations.slivers.length-1 ){ //but only if there is still text left to show
+  if ( !autoplay && clicking && clicks <= observations.slivers.length-1 ){ //but only if there is still text left to show
     slivers.forEach( element => element.clicked(mouseX, mouseY) );
   } //else do nothing
 }
@@ -146,8 +230,12 @@ function giveInfo(){ //TODO: infobutton, or maybe not necessary?
 //The code in play() is copied from Give me a Reason, which was written by
 //Tero Marttila in August of 2016 when we refactored my otherwise functional code:
 //of course it has been adapted here to the necessities of this work:
-function play(chosen){
+function play(chosen){ //so you are passing it an actual object from the json file
+//this function is creating a new audio element each time play() is called,
+//meaning that duplicates are made everytime the poem begins again,
+//instead of pointing to one of the existing 300 audio elements which would be much smarter and useful
 
+//instead: populate the audioDIV immediately (at setup or preload),
 
   var audioDiv = $('#audio');
   //audioDiv.empty(); //this stops all audio that is currently playing and starts playing the new audio
@@ -201,6 +289,22 @@ function sliver(x, y){
   this.d = 2*this.r;
   this.fill = 0;
   this.life = random(0, graypoint-50);
+
+
+if ( playpoem ){
+    if ( playpoemCounter == observations.playpoem.length ){
+      playpoemCounter = 0;
+    }
+    this.chosen = observations.playpoem[ playpoemCounter ]; // selects an object from the json file,
+    playpoemCounter++;
+
+}
+
+  //as a counter we will use slivers.length,
+  //because when we are creating the slivers,
+  //we know the position of this sliver in the array by checking against
+  //slivers.length
+
   //this.life = random(0, 20); //takes a veeery long time until grays start to appear
   //this.life = random(0, graypoint); //grays appear immediately
 
@@ -214,9 +318,15 @@ function sliver(x, y){
       if( Math.round(this.life) === graypoint && clicks == -1){
         clicks++;
         this.fill = (226,226,226); //snap the firts gray hair to light gray immediately to catch attention
+
+        if ( autoplay ){
+          readMe();
+        }
+
       } else if ( clicks == 0 && iterator == 1 ) {
     //} else if ( clicks == 1 && iterator == 1 ) { //when the text starts with an array instead of the title: Gray hairs (2023)
         this.fill = (37,37,37);
+
       } else {
          if(clicks < ( (2/3)*(observations.slivers.length) ) ){ //if the user has clicked through less than 2/3rds of the narrative
           //this.fill+=0.5; //fade the hairs more slowly
@@ -233,12 +343,21 @@ function sliver(x, y){
 
   this.clicked = function (x,y) {
 
+    if ( !x && !y ){
+      x = this.x;
+      y = this.y;
+    }
     let me = createVector(this.x, this.y);
     let mouse = createVector(x, y);
     let dist = me.dist(mouse);
     let chosen;
 
     if( dist <= this.r && this.life >= graypoint ) { //if taps within the radius of the circle
+
+      if ( this.chosen ){
+        chosen = this.chosen; //if chosen has been set initially, use it,
+        //else choose the next one in the list according to clicks and branching and arrays...
+      } else {
 
       //TODO: make clicks start at -1! It is easier for the array index...
       if( Array.isArray( observations.slivers[clicks] ) ){
@@ -287,6 +406,10 @@ function sliver(x, y){
         chosen = observations.slivers[clicks];
         clicks++;
       }
+
+        }
+
+
       //let p = createP(">> " + chosen.text); //originally: include ">>" before text
       //at this point the text has been chosen:
 
@@ -323,7 +446,7 @@ function sliver(x, y){
 
       }//end if ( text )
 
-      if ( sound ){
+      if ( sound && chosen.filename ){
         play(chosen);
       }//end if ( sound )
 
