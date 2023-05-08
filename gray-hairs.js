d49b50f99222b41bdca3194d5e69bb48b3fcb5c1
trhi <terhim@gmail.com>
2023-05-08 16:36:26 +0100

    clean up code, change variable names from dyeing/slivers -> gray hairs/hairs, delete duplicates in observations.json, comment code properly, add artist statement in comments

--- gray-hairs.js
+++ gray-hairs.js
@@ -1,62 +1,68 @@
-let sliverSize = 10, graypoint = 250; //150
+/*
+*  Gray hairs (2023)
+*  by Terhi Marttila
+*
+*  Gray hairs is about the gendered practice of dyeing one's hair.
+*  A collection of fragments/observations collected in conversation with people.
+*  Performed through the voice of Terhi Marttila under a collective "I".
+*
+*  Project credits and acknowledgments:
+*
+*  Terhi Marttila: concept, programming, texts, voice
+*  Tero Marttila: for code written in 2016 for Give Me a Reason that has been
+*  simplified to create the function loadAudios()
+*  Diogo Cocharro: for recording equipment (audio interface, microphone, stand)
+*  Silvia Freitas and Celeste Pedro: for support, encouragement and testing
+*  Amanda Hodes and editorial team: for accepting Gray hairs for publication in
+*  the Spring 2023 issue of the New River Journal
+*
+*  This work is part of Terhi's post doctoral artistic research at IST/ID.
+*  This research was funded by the Portuguese Recovery and Resilience Program
+*  (PRR), IAPMEI/ANI/FCT under Agenda C645022399-00000057 (eGamesLab).
+*
+*  You are not allowed to copy this work as a whole and present it on a website
+*  without my consent. You are not allowed to copy and use the texts. You are
+*  not allowed to copy and use the audio recordings.
+*
+*  The code itself and the concept, ie: fading clickable circles and associated
+*  texts or audio recordings as well as the functionality of the menu buttons is licenced
+*  under CC BY 4.0 - ie. you are welcome to copy any of this code and use it in
+*  whatever way you want. I appreciate if you mention my work as an inspiration
+*  or a reference. Please contact me to share any work that is in part inspired
+*  by or based on my work, I will be happy to hear about it.
+*
+*/
+
+let hairSize = 10, graypoint = 250; //150
 let xover = 6/8, yover = 8/10;
-let clicks = -1, observations, slivers = [], text = true, sound = true, autoplay = false, playing=false, started = false, linear = true; //clicks=-1 to make it easier to work with the array indexes..;
-let iterator = 0, clicking = true, playpoem = false; //TODO: not using clicks, iterator, clicking? TODO: check if using playpoem and playing?
-let chosenBranch, branchIterator = 0, autoplayCounter = 0; //TODO: remove these, no longer used?
-let playpoemCounter = 0, linearClicks = 0; //used for attributing audio elements to the hairs, used to keep track of where the user is in the reading of the linear poem
-let infoButton;
+let clicks = -1, observations, hairs = [], text = true, sound = true, autoplay = false, autoplayCounter = 0, playing=false, started = false, linear = true; //clicks=-1 to make it easier to work with the array indexes..;
+let fragmentCounter = 0, linearClicks = 0; //used for attributing audio elements to the hairs, used to keep track of where the user is in the reading of the linear poem
 
 function preload() {
-  observations = loadJSON('assets/audio/slivers.json', loadAudios );
+  observations = loadJSON('assets/audio/observations.json', loadAudios );
   //loadAudios as callback of loadJSON
   //ie. loadJSON, and once that asynchronous process is done,
   //loadAudios (ie. populate audioDiv with all the audio elements that you got from the JSON)
 
-function loadAudios(){
-    var audioDiv = $('#audio');
-    var loadingAudio = 0; //may not need this variable... letting users layer audio on top of eachother
-    var playingAudio = 0; //may not need this variable either, because letting users layer audio on top of eachother.
-    //don't really need either of these since loadAudios is happening inside preload
-    //meaning that setup() will not run until all asynchronous functions within preload() have resolved their promises?
-
-    for ( let i=0; i<observations.playpoem.length; i++ ){
-      var audio = $( "<audio></audio>", {
-        id: i, //in order to later maps hairs to audio elements
-        src: observations.path+observations.playpoem[i].filename,
-        txt: observations.playpoem[i].text, //in order to get the hair-related text directly out of the audio element
-        preload: 'auto',
-        on: {
-          canplaythrough: function(event){
-            loadingAudio--;
-            if(loadingAudio === 0){
-              //this.play(); //comment this out for now, because we create all audio elements first, play them later..
-            }
-          },//close canplaythrough:
-          ended: function(event){
-            playingAudio++;
-            clicking = true; //not using this atm:
-            //can use this to enable/disable clicking while audio is playing,
-            //but decided to enable clicking always because its nice to layer the audio
-            //TODO: remove this/itself from the audioDiv?
-            //^^ no longer necessary: creating a maximum of 300 audio elements
-
-          }//close ended:
-        }//close on:
-      });//close var audio
-      audioDiv.append(audio);
-    }
-  }//close loadAudios()
-
-
-}
-
-//conceptually: I like the idea of this being something where you just click
-//through and you hear the musings... eventually, the musings run out -
-//the musings are silenced and the gray hairs take over,
-//there are so many of them that you can no longer pluck
-//you give in. You let it go gray.
-//yet: in the silence of the musings, there is the chorus of the voice
-//dreaming of voice recordings and audio in next iteration of this work
+  function loadAudios(){
+      var audioDiv = $('#audio');
+      for ( let i=0; i<observations.fragments.length; i++ ){
+        var audio = $( "<audio></audio>", {
+          id: i, //in order to later maps hairs to audio elements
+          src: observations.path+observations.fragments[i].filename,
+          txt: observations.fragments[i].text, //in order to get the hair-related text directly out of the audio element
+          preload: 'auto',
+          on: {
+            canplaythrough: function(event){//don't do anything
+            },//close canplaythrough:
+            ended: function(event){//don't do anything
+            }//close ended:
+          }//close on:
+        });//close var audio
+        audioDiv.append(audio);
+      }
+    }//close loadAudios()
+}//close preload()
 
 function windowResized() {
   location.reload();
@@ -71,31 +77,29 @@ function setup() {
   if ( window.innerWidth >= 992 ){
     xover = 9/10; //allow clicks further into the right side of the screen for larger screens
   } else {
-    sliverSize = window.innerWidth/27; // changed to 27 to fit all 300 fragments, was /20:
-    //if small screen, tweak sliverSize to make tappable
+    hairSize = window.innerWidth/27; // changed to 27 to fit all 300 fragments, was /20:
+    //if small screen, tweak hairSize to make tappable
   }
 
-  xover *= windowWidth; //calculate the sliver of screen where text should not be displayed
+  xover *= windowWidth; //calculate the portion of screen where text should not be displayed
   yover *= windowHeight; //ditto
 
-  let sliversX = windowWidth/(sliverSize*2); //how many follicles fit horizontally?
-  let sliversY;
+  let hairsX = windowWidth/(hairSize*2-0.2); //how many follicles fit horizontally?
+  let hairsY;
 
   if( window.innerWidth >= 992 ){
-    sliversY = (windowHeight-50)/(sliverSize*2); //how many follicles fit vertically?
-    //cutoff point so that slivers are not created under menu bar
+    hairsY = (windowHeight-45)/(hairSize*2); //how many follicles fit vertically?
+    //cutoff point so that hairs are not created under menu bar
   } else {
-    sliversY = (windowHeight-75)/(sliverSize*2); //how many follicles fit vertically?
-    //cutoff point so that slivers are not created under menu bar
+    hairsY = (windowHeight-70)/(hairSize*2); //how many follicles fit vertically?
+    //cutoff point so that hairs are not created under menu bar
   }
 
-  for (let i=0; i<sliversY+1; i++){
-    for (let j=0; j<sliversX;j++){
+  for (let i=0; i<hairsY+1; i++){
+    for (let j=0; j<hairsX;j++){
       //create an array of hair follicles
       //with x and y coordinates that populate screen
-      slivers.push( new sliver( 2*sliverSize*j + sliverSize/2, 2*sliverSize*i + sliverSize/2 ) );
-      // 2.5*sliverSize*j makes them look like strands of hair
-      //it was  + sliverSize/2 for both x and y in order to make the first and last hairs go off screen
+      hairs.push( new hair( 2*hairSize*j + hairSize/2, 2*hairSize*i + hairSize/2 ) );
     }
   }
 }//close setup
@@ -112,12 +116,12 @@ function readMe() {
     let autoplaying;
 
     if ( !linear ){
-      autoplaying = random(slivers); //chooses random slivers to play
+      autoplaying = random(hairs); //chooses random hairs to play
     } else {
-      if ( autoplayCounter == slivers.length ){
+      if ( autoplayCounter == hairs.length ){
         autoplayCounter = 0;
       }
-      autoplaying = slivers[autoplayCounter];
+      autoplaying = hairs[autoplayCounter];
       autoplayCounter++;
     }
     if (autoplaying.life > graypoint){
@@ -153,15 +157,11 @@ function paintBucket(){
   setTimeout( () => { started=true; readMe(); }, 3000); //take a 3s break
   //to give time for hairs to go gray
   //so that readMe does not go into infinite loop after dyeing hairs
-  for( let i=0; i < slivers.length; i++ ){
-    slivers[i].fill = 0; //paint all hairs black
-    slivers[i].life = random(0, graypoint-10); //give them a new life, quite close to graypoint
+  for( let i=0; i < hairs.length; i++ ){
+    hairs[i].fill = 0; //paint all hairs black
+    hairs[i].life = random(0, graypoint-10); //give them a new life, quite close to graypoint
    }
-   //alterantely, use this: which also let's you tap black hairs (because they are aged even though they are black..)
-   //ie. this does not set the life of the hairs backwards:
-  //for( let i=0; i < slivers.length; i++ ){ slivers[i].fill = 0; slivers[i].life = random(0, graypoint-50); } //effectively silences the poem
 }
-
 
 function draw() {
   if( autoplay == false ){
@@ -187,30 +187,28 @@ function draw() {
   }
 
   clear();
-  drawSlivers();
+  drawHairs();
   // what color is the pixel/hair that the user is mousing over?
   let pixelColor = get(mouseX, mouseY)[0] + get(mouseX, mouseY)[3];
   if ( pixelColor == 255 || pixelColor == 0 ){
     cursor(ARROW); //show normal arrow for black hairs
   } else {
-      cursor(CROSS); //show crosshair for graying hairs
+    cursor(CROSS); //show crosshair for graying hairs
   }
 }
 
-function drawSlivers() {
-  for( let i=0; i < slivers.length; i++ ){ slivers[i].display(); }
+function drawHairs() {
+  for( let i=0; i < hairs.length; i++ ){ hairs[i].display(); }
 }
 
 function mousePressed() {
   if ( !autoplay ){ //clicking is disabled when in autoplay
 //if ( !autoplay || autoplay ){ //clicking is always enabled
-    slivers.forEach( element => element.clicked(mouseX, mouseY) );
+    hairs.forEach( element => element.clicked(mouseX, mouseY) );
   }
 }//close mousePressed
 
-
-
-function sliver(x, y){
+function hair(x, y){
 
   if(!x || !y){ //if no x OR y given when they are created, put them in a random position:
     x = random(0, windowWidth);
@@ -219,18 +217,18 @@ function sliver(x, y){
 
   this.x = x;
   this.y = y;
-  this.r = sliverSize;
+  this.r = hairSize;
   this.d = 2*this.r;
   this.fill = 0;
   this.life = random(0, graypoint-50);
   //this.life = random(0, 20); //takes a veeery long time until grays start to appear
   //this.life = random(0, graypoint); //grays appear immediately
 
-  if ( playpoemCounter == observations.playpoem.length ){
-    playpoemCounter = 0;
+  if ( fragmentCounter == observations.fragments.length ){
+    fragmentCounter = 0;
   }
-  this.audio = $("#" + playpoemCounter);
-  playpoemCounter++;
+  this.audio = $("#" + fragmentCounter);
+  fragmentCounter++;
 
   this.display = function() {
     this.life++;
@@ -240,7 +238,7 @@ function sliver(x, y){
         this.fill = (226,226,226); //snap the firts gray hair to light gray immediately to catch attention
         started = true;
       } else {
-         if(clicks < ( (2/3)*(observations.slivers.length) ) ){ //if the user has clicked through less than 2/3rds of the narrative
+         if(clicks < ( (2/3)*(observations.fragments.length) ) ){ //if the user has clicked through less than 2/3rds of the narrative
            if ( clicks == 0){
            } else {
              this.fill = (37,37,37); //starts the initial graying faster
@@ -272,18 +270,14 @@ function sliver(x, y){
 
       //then check whether linear mode is on:
       if ( linear ){
-        if( linearClicks == observations.playpoem.length ){
+        if( linearClicks == observations.fragments.length ){
           linearClicks = 0;
         }
-        //associate a onetime "chosen" to an audio element instead of assigning it to this.audio
-        //when the user has linear narrative mode selected
-        //TODO: remove this.chosen (because chosen/linear is a onetime variable
-        //so no need to update it to the sliver object itself)
         chosen = $("#"+linearClicks);
         linearClicks++;
       } else {
         //if its not in linear mode, then clicking a dot means that
-        //chosen - ie. what we will soon play, is this.audio.
+        //chosen - ie. what we will soon play - is this.audio.
         chosen = this.audio;
       }
 
@@ -308,7 +302,7 @@ function sliver(x, y){
             p.position(this.x, this.y);
           }
           //we are no longer counting clicks... so this is not doing anything:
-          if ( linear && linearClicks == observations.playpoem.length ){ //if in linear, fade out: the end very very slowly
+          if ( linear && linearClicks == observations.fragments.length ){ //if in linear, fade out: the end very very slowly
             $( "p" ).fadeOut( 12345 ); //fade the last word out very slowly
           } else {
             $( "p" ).fadeOut( 2500 ); //fade other text out after normal interval
@@ -326,7 +320,6 @@ function sliver(x, y){
       this.fill = 0;
       this.life = random(0,20);
     }//close if (tap within this hair)
-
   }//close this.clicked
 
-}//close sliver()
+}//close hair()
